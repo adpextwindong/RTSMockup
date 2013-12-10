336c322a24839df8ab4474c86dfa508480abbdbe
adpextwindong <adpextwindng@gmail.com>
2013-12-10 09:43:13 -0800

    Naming Convention
    c_xxx for client
    s_xxx for server

--- RTSMockup.cpp
+++ RTSMockup.cpp
@@ -12,7 +12,7 @@
 
 enum DecisionState {Selecting,Commanding};
  
-sf::RectangleShape selectionShape;
+sf::RectangleShape c_clientSelectionShape;
 sf::Vector2i originalPoint;//SELECT LEFT CLICK POINT
 sf::RenderWindow window(sf::VideoMode(1024, 768), "RTS Mockup");
 
@@ -42,68 +42,68 @@ void updateSelection(){
 	int yDiff=(LimitedMousePos.y-originalPoint.y);
 	if((LimitedMousePos.x-originalPoint.x)<=0){//Xdiff +x
 		if(yDiff<=0){//+x +y Quad 1
-			selectionShape.setPosition(originalPoint.x,LimitedMousePos.y);
-			selectionShape.setSize(sf::Vector2f(LimitedMousePos.x-originalPoint.x,originalPoint.y-LimitedMousePos.y));
+			c_clientSelectionShape.setPosition(originalPoint.x,LimitedMousePos.y);
+			c_clientSelectionShape.setSize(sf::Vector2f(LimitedMousePos.x-originalPoint.x,originalPoint.y-LimitedMousePos.y));
 		}else{//+x -y Quad 4
-			selectionShape.setSize(sf::Vector2f(LimitedMousePos.x-originalPoint.x,LimitedMousePos.y-originalPoint.y));
+			c_clientSelectionShape.setSize(sf::Vector2f(LimitedMousePos.x-originalPoint.x,LimitedMousePos.y-originalPoint.y));
 		}
 	}else{
 		if(yDiff<=0){// -x +y Quad 2
-			selectionShape.setPosition(sf::Vector2f(LimitedMousePos));
-			selectionShape.setSize(sf::Vector2f(originalPoint.x-LimitedMousePos.x,originalPoint.y-LimitedMousePos.y));
+			c_clientSelectionShape.setPosition(sf::Vector2f(LimitedMousePos));
+			c_clientSelectionShape.setSize(sf::Vector2f(originalPoint.x-LimitedMousePos.x,originalPoint.y-LimitedMousePos.y));
 		}else{// -x -y Quad 3
-			selectionShape.setPosition(sf::Vector2f(LimitedMousePos.x,originalPoint.y));
-			selectionShape.setSize(sf::Vector2f(originalPoint.x-LimitedMousePos.x,LimitedMousePos.y-originalPoint.y));
+			c_clientSelectionShape.setPosition(sf::Vector2f(LimitedMousePos.x,originalPoint.y));
+			c_clientSelectionShape.setSize(sf::Vector2f(originalPoint.x-LimitedMousePos.x,LimitedMousePos.y-originalPoint.y));
 		}
 	}
 
-	//printf("Shape Pos:%f %f\n",selectionShape.getPosition().x,selectionShape.getPosition().y);
+	//printf("Shape Pos:%f %f\n",c_clientSelectionShape.getPosition().x,c_clientSelectionShape.getPosition().y);
 	for(int i=0;i<4;i++){
-		//printf("P(%d): %.0f,%.0f ",i,selectionShape.getPoint(i).x+selectionShape.getPosition().x,selectionShape.getPoint(i).y+selectionShape.getPosition().y);
+		//printf("P(%d): %.0f,%.0f ",i,c_clientSelectionShape.getPoint(i).x+c_clientSelectionShape.getPosition().x,c_clientSelectionShape.getPoint(i).y+c_clientSelectionShape.getPosition().y);
 	}
 	//printf("\n\n");
 
 	//printf("Limited MosPos %d %d ",LimitedMousePos.x,LimitedMousePos.y);
 	//printf("Real MosPos %d %d\n",sf::Mouse::getPosition(window).x,sf::Mouse::getPosition(window).y);
 	/*printf("Original Point %d %d ",originalPoint.x,originalPoint.y);
-	printf("Selection Cords %f.0 %f.0\n\n",selectionShape.getPosition().x,selectionShape.getPosition().y);*/
+	printf("Selection Cords %f.0 %f.0\n\n",c_clientSelectionShape.getPosition().x,c_clientSelectionShape.getPosition().y);*/
 }
 unsigned int counter=0;
-void grabOnScreenSelectedUnits(std::vector<Unit>* playerUnits,std::vector<Unit *>* playerSelection){
+void grabOnScreenSelectedUnits(std::vector<Unit>* s_playerUnits,std::vector<Unit *>* c_playerSelection){
 	bool shifted = false;
 	unsigned int preExistingElemCount=0;
 	if(!sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)&&!sf::Keyboard::isKeyPressed(sf::Keyboard::RShift)){
 		printf("UnShifted %S Command \n","Selection");
-		(*playerSelection).clear();
+		(*c_playerSelection).clear();
 		//printf("\nList cleared\n");
 	}else{
 		printf("Shifted %S Command \n","Selection");
 		shifted=true;
-		preExistingElemCount=(*playerSelection).size();
+		preExistingElemCount=(*c_playerSelection).size();
 	}
-	float minX=selectionShape.getPoint(0).x+selectionShape.getPosition().x;
+	float minX=c_clientSelectionShape.getPoint(0).x+c_clientSelectionShape.getPosition().x;
 	float maxX=0.f;
-	float minY=selectionShape.getPoint(0).y+selectionShape.getPosition().y;
+	float minY=c_clientSelectionShape.getPoint(0).y+c_clientSelectionShape.getPosition().y;
 	float maxY=0.f;
 	for(int i=0;i<4;i++){
-		minX = minX > selectionShape.getPoint(i).x+selectionShape.getPosition().x ? selectionShape.getPoint(i).x+selectionShape.getPosition().x : minX;
-		maxX = maxX < selectionShape.getPoint(i).x+selectionShape.getPosition().x ? selectionShape.getPoint(i).x+selectionShape.getPosition().x : maxX;
-		minY = minY > selectionShape.getPoint(i).y+selectionShape.getPosition().y ? selectionShape.getPoint(i).y+selectionShape.getPosition().y : minY;
-		maxY = maxY < selectionShape.getPoint(i).y+selectionShape.getPosition().y ? selectionShape.getPoint(i).y+selectionShape.getPosition().y : maxY;
+		minX = minX > c_clientSelectionShape.getPoint(i).x+c_clientSelectionShape.getPosition().x ? c_clientSelectionShape.getPoint(i).x+c_clientSelectionShape.getPosition().x : minX;
+		maxX = maxX < c_clientSelectionShape.getPoint(i).x+c_clientSelectionShape.getPosition().x ? c_clientSelectionShape.getPoint(i).x+c_clientSelectionShape.getPosition().x : maxX;
+		minY = minY > c_clientSelectionShape.getPoint(i).y+c_clientSelectionShape.getPosition().y ? c_clientSelectionShape.getPoint(i).y+c_clientSelectionShape.getPosition().y : minY;
+		maxY = maxY < c_clientSelectionShape.getPoint(i).y+c_clientSelectionShape.getPosition().y ? c_clientSelectionShape.getPoint(i).y+c_clientSelectionShape.getPosition().y : maxY;
 	}
 	float radius=0.f;
-	for(unsigned int unitIterator=0;unitIterator<(*playerUnits).size();unitIterator++){//Iterate through all units
+	for(unsigned int unitIterator=0;unitIterator<(*s_playerUnits).size();unitIterator++){//Iterate through all units
 		Unit * currentPointer=NULL;
 		bool containsThePointer = false;
 		if(shifted==true){//If user is adding, check if current unit is on the list already
-			currentPointer =&(*playerUnits)[unitIterator];
-			for(unsigned int j=0;j<(*playerSelection).size();j++){//iterate through unit selection vector
-				if(currentPointer==(*playerSelection)[j]){
+			currentPointer =&(*s_playerUnits)[unitIterator];
+			for(unsigned int j=0;j<(*c_playerSelection).size();j++){//iterate through unit selection vector
+				if(currentPointer==(*c_playerSelection)[j]){
 					containsThePointer=true;
-					if(selectionShape.getSize().x ==0 && selectionShape.getSize().y ==0){
-						if(sqrt(std::pow((*playerUnits)[unitIterator].UnitShape.getRadius()+(*playerUnits)[unitIterator].UnitShape.getPosition().x-selectionShape.getPosition().x,2.0f)+std::pow((*playerUnits)[unitIterator].UnitShape.getRadius()+(*playerUnits)[unitIterator].UnitShape.getPosition().y-selectionShape.getPosition().y,2.0f))<=(*playerUnits)[unitIterator].UnitShape.getRadius()){
-							(*playerSelection).erase((*playerSelection).begin()+j);
-							unitIterator=(*playerUnits).size()+1;
+					if(c_clientSelectionShape.getSize().x ==0 && c_clientSelectionShape.getSize().y ==0){
+						if(sqrt(std::pow((*s_playerUnits)[unitIterator].UnitShape.getRadius()+(*s_playerUnits)[unitIterator].UnitShape.getPosition().x-c_clientSelectionShape.getPosition().x,2.0f)+std::pow((*s_playerUnits)[unitIterator].UnitShape.getRadius()+(*s_playerUnits)[unitIterator].UnitShape.getPosition().y-c_clientSelectionShape.getPosition().y,2.0f))<=(*s_playerUnits)[unitIterator].UnitShape.getRadius()){
+							(*c_playerSelection).erase((*c_playerSelection).begin()+j);
+							unitIterator=(*s_playerUnits).size()+1;
 						}
 					}
 					break;
@@ -111,16 +111,16 @@ void grabOnScreenSelectedUnits(std::vector<Unit>* playerUnits,std::vector<Unit *
 			}
 		}
 		if(containsThePointer==false){//if this element isn't in the list
-			if(selectionShape.getSize().x ==0 && selectionShape.getSize().y ==0){
-				if(sqrt(std::pow((*playerUnits)[unitIterator].UnitShape.getRadius()+(*playerUnits)[unitIterator].UnitShape.getPosition().x-selectionShape.getPosition().x,2.0f)+std::pow((*playerUnits)[unitIterator].UnitShape.getRadius()+(*playerUnits)[unitIterator].UnitShape.getPosition().y-selectionShape.getPosition().y,2.0f))<=(*playerUnits)[unitIterator].UnitShape.getRadius()){
-					(*playerSelection).push_back(&(*playerUnits)[unitIterator]);
+			if(c_clientSelectionShape.getSize().x ==0 && c_clientSelectionShape.getSize().y ==0){
+				if(sqrt(std::pow((*s_playerUnits)[unitIterator].UnitShape.getRadius()+(*s_playerUnits)[unitIterator].UnitShape.getPosition().x-c_clientSelectionShape.getPosition().x,2.0f)+std::pow((*s_playerUnits)[unitIterator].UnitShape.getRadius()+(*s_playerUnits)[unitIterator].UnitShape.getPosition().y-c_clientSelectionShape.getPosition().y,2.0f))<=(*s_playerUnits)[unitIterator].UnitShape.getRadius()){
+					(*c_playerSelection).push_back(&(*s_playerUnits)[unitIterator]);
 				}
 			}else{
-				radius=(*playerUnits)[unitIterator].UnitShape.getRadius()/2;
-				sf::Vector2f position((*playerUnits)[unitIterator].position.x,(*playerUnits)[unitIterator].position.y);
+				radius=(*s_playerUnits)[unitIterator].UnitShape.getRadius()/2;
+				sf::Vector2f position((*s_playerUnits)[unitIterator].position.x,(*s_playerUnits)[unitIterator].position.y);
 				if(position.x >= minX && position.x <= maxX){
 					if(position.y >= minY && position.y <= maxY){
-						(*playerSelection).push_back(&(*playerUnits)[unitIterator]);
+						(*c_playerSelection).push_back(&(*s_playerUnits)[unitIterator]);
 					}
 				}
 			}
@@ -143,14 +143,14 @@ Unit * unitAtMousePos(){//TODO: FINISH THIS FUNCTION
 	//check for target unit at mouse posisiton
 	return NULL;
 }
-void CommandSelectionUnits(CommandEnum theCommand, std::vector<Unit *> * playerSelection){
-	for(unsigned int i=0;i<(*playerSelection).size();i++){
+void CommandSelectionUnits(CommandEnum theCommand, std::vector<Unit *> * c_playerSelection){
+	for(unsigned int i=0;i<(*c_playerSelection).size();i++){
 		if(!sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)&&!sf::Keyboard::isKeyPressed(sf::Keyboard::RShift)){
 			printf("UnShifted %S Command",commandString(theCommand));
-			(*(*playerSelection)[i]).unitCommands.empty();
+			(*(*c_playerSelection)[i]).unitCommands.empty();
 		}
 		printf("Shifted %S Command",commandString(theCommand));
-		(*(*playerSelection)[i]).unitCommands.push_back(Command(theCommand,Point2D(sf::Mouse::getPosition(window)),unitAtMousePos()));
+		(*(*c_playerSelection)[i]).unitCommands.push_back(Command(theCommand,Point2D(sf::Mouse::getPosition(window)),unitAtMousePos()));
 	}
 }
 
@@ -165,19 +165,19 @@ bool mouseIsOnScreen(){
 	}
 	return false;
 }
-void drawSelectionStroke(std::vector<Unit*>* playerSelection){
+void drawSelectionStroke(std::vector<Unit*>* c_playerSelection){
 	sf::CircleShape strokeShape(0.f);
 	strokeShape.setFillColor(SELECTION_STROKE_COLOR);
-	//printf(" %d\n",(*playerSelection).size());
-	for(unsigned int i=0;i<(*playerSelection).size();i++){
-		strokeShape.setRadius((*(*playerSelection)[i]).UnitShape.getRadius()+STROKE_SIZE);
-		strokeShape.setPosition((*(*playerSelection)[i]).UnitShape.getPosition().x-STROKE_SIZE,(*(*playerSelection)[i]).UnitShape.getPosition().y-STROKE_SIZE);
+	//printf(" %d\n",(*c_playerSelection).size());
+	for(unsigned int i=0;i<(*c_playerSelection).size();i++){
+		strokeShape.setRadius((*(*c_playerSelection)[i]).UnitShape.getRadius()+STROKE_SIZE);
+		strokeShape.setPosition((*(*c_playerSelection)[i]).UnitShape.getPosition().x-STROKE_SIZE,(*(*c_playerSelection)[i]).UnitShape.getPosition().y-STROKE_SIZE);
 		window.draw(strokeShape);
 	}
 }
-//void pushBackSelectionToControlGroup(std::vector<Unit *>* playerSelection,std::vector<Unit>* playerUnits){//for control groups
-//	for(unsigned int i=0;i<(*playerUnits).size();i++){
-//		if((*playerUnits)[i].position.x-(*playerUnits)[i].UnitShape.getRadius()>=
+//void pushBackSelectionToControlGroup(std::vector<Unit *>* c_playerSelection,std::vector<Unit>* s_playerUnits){//for control groups
+//	for(unsigned int i=0;i<(*s_playerUnits).size();i++){
+//		if((*s_playerUnits)[i].position.x-(*s_playerUnits)[i].UnitShape.getRadius()>=
 //	}
 //		
 //}
@@ -187,18 +187,22 @@ int _tmain(int argc, _TCHAR* argv[])
 	//Encrypt packets
 	//or use Base64 and obsfuscate
 	
-	sf::ContextSettings settings;
-	settings.antialiasingLevel=8;
+	//Mockup naming conventions
+	//c_xxx for client side stuff
+	//s_xxx for game state that will be server side in the future
 
-	std::vector<Unit> playerUnits;
-	std::vector<Unit> enemyUnits;
-	std::vector<Unit *> playerSelection;
+	sf::ContextSettings c_settings;
+	c_settings.antialiasingLevel=8;
+
+	std::vector<Unit> s_playerUnits;
+	std::vector<Unit> s_enemyUnits;
+	std::vector<Unit *> c_playerSelection;
 
 	for(unsigned int i=0;i<5;i++){
-		playerUnits.push_back(Unit(Point2D((i+1)*150+50,668),sf::Color::Green,10));
+		s_playerUnits.push_back(Unit(Point2D((i+1)*150+50,668),sf::Color::Green,10));
 	}
 	for(unsigned int i=0;i<3;i++){
-		enemyUnits.push_back(Unit(Point2D((i+1)*150+180,100),sf::Color::Red,20));
+		s_enemyUnits.push_back(Unit(Point2D((i+1)*150+180,100),sf::Color::Red,20));
 	}
 	//Allows the player to queue up commands.
 	//Non Shifted Commands empty the list.
@@ -207,12 +211,11 @@ int _tmain(int argc, _TCHAR* argv[])
 	DecisionState mouseCommandState = Selecting;//True if valid command key is pressed.
 	CommandEnum currentCommand;
 	bool leftMouseClickedLastCycle = false;//For box selection
-	
 
-	selectionShape.setFillColor(sf::Color::Transparent);
-	selectionShape.setOutlineThickness(3.0f);
-	selectionShape.setOutlineColor(sf::Color::Black);
-	selectionShape.setSize(sf::Vector2f(0.f,0.f));
+	c_clientSelectionShape.setFillColor(sf::Color::Transparent);
+	c_clientSelectionShape.setOutlineThickness(3.0f);
+	c_clientSelectionShape.setOutlineColor(sf::Color::Black);
+	c_clientSelectionShape.setSize(sf::Vector2f(0.f,0.f));
 
 	bool selectionDrawState = false; //user is selecting if true
 
@@ -224,12 +227,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	//Valid Commands: Attack, Move, Halt, Stop
 	//LEFT CLICK DOES TWO THINGS. SELECTION AND COMMANDING
 	//RIGHT CLICK ONLY DOES MOVE COMMAND
-	bool inWindow =false;
-	//std::string numberOfUnits="Number of Units Selected";
-	//unsigned int counter=0;
-	//numberOfUnits+std::to_string((static_cast<long long>( playerSelection.size())));
-	//http://stackoverflow.com/questions/10664699/stdto-string-more-than-instance-of-overloaded-function-matches-the-argument/10666695#10666695
-	//Oh wow a c++ static cast. Not my idea. VS2010 just doesn't fit the c++11 standard
+
 	sf::RectangleShape background(sf::Vector2f(window.getSize().x,window.getSize().y));
 	background.setFillColor(sf::Color::White);
     while (window.isOpen())
@@ -245,34 +243,34 @@ int _tmain(int argc, _TCHAR* argv[])
 			if(leftMouseClickedLastCycle==true){//if user has released left click button
 				switch(mouseCommandState){
 				case Selecting://Selection Change
-					//pushBackSelection(&playerSelection,&playerUnits);
+					//pushBackSelection(&c_playerSelection,&s_playerUnits);
 					
-					grabOnScreenSelectedUnits(&playerUnits,&playerSelection);
-					printf("%d\n",playerSelection.size());
+					grabOnScreenSelectedUnits(&s_playerUnits,&c_playerSelection);
+					printf("%d\n",c_playerSelection.size());
 					selectionDrawState=false;
 					break;
 				case Commanding:
-					CommandSelectionUnits(currentCommand,&playerSelection);
+					CommandSelectionUnits(currentCommand,&c_playerSelection);
 					break;
 				}
 			}else{//if user has clicked left click button
 				if(mouseIsOnScreen()){
 					switch(mouseCommandState){
 					case Selecting:
-						selectionShape.setSize(sf::Vector2f(0,0));
+						c_clientSelectionShape.setSize(sf::Vector2f(0,0));
 						originalPoint=sf::Mouse::getPosition(window);
 						updateSelection();
 						selectionDrawState=true;
 						break;
 					case Commanding:
-						CommandSelectionUnits(currentCommand,&playerSelection);
+						CommandSelectionUnits(currentCommand,&c_playerSelection);
 						break;
 					}
 				}
 			}
 			leftMouseClickedLastCycle^=true;//Swaps leftMouseClickLastCycle state
 		}else if(sf::Mouse::isButtonPressed(sf::Mouse::Right)){//If Right mbutton clicked
-			CommandSelectionUnits(Move,&playerSelection);
+			CommandSelectionUnits(Move,&c_playerSelection);
 		}
 		
 		if(sf::Mouse::isButtonPressed(sf::Mouse::Left)==true &&leftMouseClickedLastCycle==true && mouseCommandState==Selecting){//If the player is still holding down the left click button while selecting
@@ -283,11 +281,11 @@ int _tmain(int argc, _TCHAR* argv[])
 		//printf("%s",selectionDrawState?"TRUE\n":"FALSE\n");
 		window.draw(background);
 		if(selectionDrawState==true){
-			window.draw(selectionShape);
+			window.draw(c_clientSelectionShape);
 		}
-		drawSelectionStroke(&playerSelection);
-		drawUnitVector(&window,playerUnits);
-		drawUnitVector(&window,enemyUnits);
+		drawSelectionStroke(&c_playerSelection);
+		drawUnitVector(&window,s_playerUnits);
+		drawUnitVector(&window,s_enemyUnits);
         window.display();
     }
 
